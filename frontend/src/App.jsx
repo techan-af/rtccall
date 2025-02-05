4ed7edae0da9362be6cb23d5beb583e01cb677c8
Chetan <chetan95497@gmail.com>
2025-02-05 23:03:56 +0530

    frontend fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://rtccall.onrender.com");
 
 export default function Home() {
     const [room, setRoom] = useState("");
