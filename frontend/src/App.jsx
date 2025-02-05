21ebed59128fe2cdda01d783876b85218c97c1e2
Chetan <chetan95497@gmail.com>
2025-02-05 23:09:56 +0530

    minor problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
 const socket = io("https://rtccall.onrender.com");
